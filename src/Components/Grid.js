e1fb8b910ab27da9923001ee37b0d87a6ebf4d05
Peter Rifkind <prifkind@gmail.com>
2022-02-09 22:25:30 -0500

    began styling grid with borders

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -4,8 +4,7 @@ import Row from "./Row";
 const Grid = ({ querySubmit, games }) => {
   useEffect(() => {
     querySubmit();
-
-  }, [querySubmit]);
+  }, []);
 
   const renderBox = games.map((g) => {
     const homeTeam = g.home_team.full_name;
@@ -16,14 +15,21 @@ const Grid = ({ querySubmit, games }) => {
     return (
       <Row
         key={id}
-        homeTeam={homeTeam}
-        visitorTeam={visitorTeam}
-        homeTeamScore={homeTeamScore}
-        visitorTeamScore={visitorTeamScore}
+        column1={homeTeam}
+        column2={visitorTeam}
+        column3={homeTeamScore}
+        column4={visitorTeamScore}
       />
     );
   });
-  return <div>{renderBox}</div>;
+  return (
+    <div>
+      <div className="ui grid">
+        <Row column1='Home' column2='Visitor' column3='Home Score' column4='Visitor Score' />
+        <div>{renderBox}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Grid;
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,15 +1,27 @@
 import React from "react";
 
-const Row = ({ homeTeam, visitorTeam, homeTeamScore, visitorTeamScore }) => {
-
+const Row = ({ column1, column2, column3, column4 }) => {
+  const styles = {
+    border: "1px solid black",
+  };
   return (
-    <div className="ui grid">
-    <div className="four wide column">{homeTeam}</div>
-    <div className="four wide column">{visitorTeam}</div>
-    <div className="four wide column">{homeTeamScore}</div>
-    <div className="four wide column">{visitorTeamScore}</div>
-  </div>
-  )
+    <div>
+      <div className="column">
+        <div style={styles}>{column1}</div>
+      </div>
+      <div className="column">
+        <div style={styles}>{column2}</div>
+      </div>
+      <div className="column">
+        <div style={styles}>{column3}</div>
+      </div>
+      <div className="column">
+        <div className="column" style={styles}>
+          {column4}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
