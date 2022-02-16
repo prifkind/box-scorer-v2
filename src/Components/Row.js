2535ab213eb7bb92cc0582db5e2a3eef67a68823
Peter Rifkind <prifkind@gmail.com>
2022-02-15 22:37:17 -0500

    no changes

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -3,7 +3,7 @@ import "../styles.css";
 
 const Row = ({ column1, column2, column3, column4 }) => {
   return (
-    <div className="grid">
+    <div className="gridFlex">
       <div className="gridRow">{column1}</div>
       <div className="gridRow">{column2}</div>
       <div className="gridRow">{column3}</div>
